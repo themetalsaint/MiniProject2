62385fc5cf595a8d0116d91df225fd7b7c096bf0
Rachael Davis <80214032+themetalsaint@users.noreply.github.com>
2021-04-17 13:49:41 -0400

    inital code

---
+++ assets/script.js
@@ -0,0 +1,15 @@
+// 1. Start Button
+// 2. Word appearing and disappearing
+// 3. Score appear & stored
+//4. Reset button
+var seconds = 10
+var randomWords = [ 'javascript', 'array', 'object', 'confusion']
+
+var startButton = document.querySelector("#starterButton")
+var resetButton = document.querySelector("#resetButton")
+var wins = document.querySelector("#winCounter")
+var losses = document.querySelector("#loseCounter")
+
+
+// var timer = document.querySelector(seconds)
+    
